5ad5566ffdf9ea4b9d76184d2fc1e5854eaa249d
yungpanko <jrjohns@mit.edu>
2017-05-24 11:44:06 -0400

    Done.

--- artist.rb
+++ artist.rb
@@ -1,7 +1,13 @@
 class Artist < ActiveRecord::Base
-
   def self.create_table
-    # create table using SQL here
-  end
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS songs (
+      id INTEGER PRIMARY KEY,
+      title TEXT,
+      length INTEGER
+    )
+    SQL
 
+    ActiveRecord::Base.connection.execute(sql)
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,10 @@ Bundler.require
 
 
 # put the code to connect to the database here
+connection = ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/songs.sqlite"
+)
 
 
 require_relative "../artist.rb"
